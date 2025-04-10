aa84c1b19e0cb5b3d7771e0786a2fb5ea474dc85
maxwell <maxweliau123456@gmail.com>
2025-04-11 02:02:33 +0800

    cleaned up util.py

--- util.py
+++ util.py
@@ -23,41 +23,30 @@ def get_connection() -> pyodbc.Connection:
 def create_table(sql_path: str) ->None:
     conn = get_connection()
     with open(sql_path, 'r', encoding='utf-8') as f:
-        # print(f"Running {sql_path}")
         sql = f.read()
-        # Create a cursor and run a simple query
         cursor = conn.cursor()
         cursor.execute(sql)
         print(f"Successfully Created Table {sql_path} ✅")
-        # Always close when done
         cursor.close()
     conn.close()
 
 def seed(sql_path: str) ->None:
     conn = get_connection()
     with open(sql_path, 'r', encoding='utf-8') as f:
-        # print(f"Running {sql_path}")
         sql = f.read()
-        # Create a cursor and run a simple query
         cursor = conn.cursor()
         cursor.execute(sql)
         print(f"Successfully Seeded {sql_path} ✅")
-        # Always close when done
         cursor.close()
     conn.close()
 
 def query(sql_path: str) ->None:
     conn = get_connection()
     with open(sql_path, 'r', encoding='utf-8') as f:
-        # print(f"Running {sql_path}")
         sql = f.read()
-        # Create a cursor and run a simple query
         cursor = conn.cursor()
         cursor.execute(sql)
-        # print(f"Queried {sql_path}")
         result = cursor.fetchall()
-        # print(result)
-        # Always close when done
         cursor.close()
         conn.close()
         return result
